a3d9d36855cba8bceb74d4b073e1d7d4254eac0f
Adriano Anschau <adrianoanschau@gmail.com>
2021-01-05 17:01:45 -0300

    Update require devs

--- src/GrazziotinApiServiceProvider.php
+++ src/GrazziotinApiServiceProvider.php
@@ -6,7 +6,6 @@ use BenSampo\Enum\EnumServiceProvider;
 use Fruitcake\Cors\CorsServiceProvider;
 use geekcom\ValidatorDocs\ValidatorProvider;
 use Illuminate\Support\ServiceProvider;
-use Knuckles\Scribe\ScribeServiceProvider;
 use Yajra\Oci8\Oci8ServiceProvider;
 
 class GrazziotinApiServiceProvider extends ServiceProvider {
@@ -16,7 +15,6 @@ class GrazziotinApiServiceProvider extends ServiceProvider {
         $this->app->register(CorsServiceProvider::class);
         $this->app->register(Oci8ServiceProvider::class);
         $this->app->register(EnumServiceProvider::class);
-        $this->app->register(ScribeServiceProvider::class);
         $this->app->register(ValidatorProvider::class);
     }
 
